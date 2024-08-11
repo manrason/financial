b2a6c0e07f4c922b00f0ec8e87c040fbaaa533fb
titibeloha <manrason2@gmail.com>
2024-08-11 19:07:54 +0200

    init project

---
+++ src/test/java/financial/tracker/financial/FinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package financial.tracker.financial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
